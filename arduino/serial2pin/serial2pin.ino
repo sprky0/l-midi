f4dae57cc875fdc616c32537034d526141f91c76
Avery Brooks <averyjamesbrooks@gmail.com>
2016-03-05 16:12:17 -0500

    offset pins in array to allow variable config

--- arduino/serial2pin/serial2pin.ino
+++ arduino/serial2pin/serial2pin.ino
@@ -1,23 +1,25 @@
-#define pinStart 0
-#define pinCount 54	// total pins
-int delayTime = 10;
+#define pinCount 32
+int pins[] = {
+	// notes 0 - 15
+	 2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 45, 53, 51, 49, 47,
+	// notes 16-31
+	22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52
+};
+int delayTime = 250;
 
 void setup() {
-	for (int curPin = pinStart; curPin < pinStart + pinCount; curPin++) {
-		pinMode(curPin, OUTPUT);           // set pin to input
-		digitalWrite(curPin, HIGH);           // set pin to input
+	for (int curPin = 0; curPin < pinCount; curPin++) {
+		pinMode(pins[curPin], OUTPUT);           // set pin to input
+		digitalWrite(pins[curPin], HIGH);           // set pin to input
 	}
-
 }
 
 void loop() {
-	/*
-	for (int curPin = pinStart; curPin < pinStart + pinCount; curPin++) {
-		digitalWrite(curPin, LOW);       // turn on pullup resistors
+	for (int curPin = 0; curPin < pinCount; curPin++) {
+		digitalWrite(pins[curPin], LOW);       // turn on pullup resistors
 		delay(delayTime * 2);
-		digitalWrite(curPin, HIGH);       // turn on pullup resistors
+		digitalWrite(pins[curPin], HIGH);       // turn on pullup resistors
 		delay(delayTime * 2);
 	}
 	delay(4000);
-	*/
 }
