d081ea75992e07a6588db305dc86d0b426f336b9
Avery Brooks <averyjamesbrooks@gmail.com>
2016-06-29 01:47:51 -0400

    lmidi-start is the non init.d version

--- scripts/lmidi-start.sh
+++ scripts/lmidi-start.sh
@@ -2,7 +2,7 @@
 cd /home/pi/l-midi/processing
 COUNTER=0
 while [ 1 ]; do
-   echo Run $COUNTER
-   DISPLAY=:0 processing-java --sketch=lmidi --present
-   let COUNTER=COUNTER+1
+    echo Run $COUNTER
+    DISPLAY=:0 processing-java --sketch=lmidi --present
+    let COUNTER=COUNTER+1
 done
