d9364cf54c1a434fa3b10cf7687adfc99a0a8254
Avery Brooks <averyjamesbrooks@gmail.com>
2016-06-29 01:18:41 -0400

    poke

--- scripts/lmidi-start.sh
+++ scripts/lmidi-start.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-su pi
 cd /home/pi/l-midi/processing
 COUNTER=0
 while [ 1 ]; do
    echo Run $COUNTER
-	 DISPLAY=:0 processing-java --sketch=lmidi --present
+   DISPLAY=:0 processing-java --sketch=lmidi --present
    let COUNTER=COUNTER+1
 done
