137e0a80e6b9d0457ee256e24cb5e7e6ceb4cbd2
Avery Brooks <averyjamesbrooks@gmail.com>
2016-06-29 01:05:05 -0400

    start script

--- scripts/lmidi-start.sh
+++ scripts/lmidi-start.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+su pi
 cd /home/pi/l-midi/processing
-DISPLAY=:0 processing-java --sketch=lmidi --present
+COUNTER=0
+while [ 1 ]; do
+   echo Run $COUNTER
+	 DISPLAY=:0 processing-java --sketch=lmidi --present
+   let COUNTER=COUNTER+1
+done
