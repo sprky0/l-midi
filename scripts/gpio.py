0f96e0fe796590bce885d03979ecfdf182c89875
Avery Brooks <averyjamesbrooks@gmail.com>
2016-07-05 07:05:22 -0400

    maybe fix what i broke in py

--- scripts/gpio.py
+++ scripts/gpio.py
@@ -23,13 +23,11 @@ def SendMessageToP5(message):
         # message = 'playpause'
         print >>sys.stderr, 'sending "%s"' % message
         sock.sendall(message)
-
-        # Look for the response
+        # # Look for the response
         amount_received = 0
-        # amount_expected = len(response)
-
+        amount_expected = len(response)
         while amount_received < amount_expected:
-            data = sock.recv(1024)
+            data = sock.recv(16)
             amount_received += len(data)
             print >>sys.stderr, 'received "%s"' % data
 
