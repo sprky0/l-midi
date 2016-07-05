7e2b51ad4622cc449685dcde0d307f4f35e0bdeb
Avery Brooks <averyjamesbrooks@gmail.com>
2016-07-05 06:29:59 -0400

    restore callback binding

--- scripts/gpio.py
+++ scripts/gpio.py
@@ -6,10 +6,6 @@ import os
 import socket
 import sys
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-GPIO.setup(24, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-
 # Our function on what to do when the button is pressed
 def Shutdown(channel):
     os.system("sudo shutdown -h now")
@@ -49,6 +45,12 @@ def NextSequence(channel):
 
 def PrevSequence(channel):
     SendMessageToP5('next');
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(24, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.add_event_detect(18, GPIO.FALLING, callback = Shutdown, bouncetime = 2000)
+GPIO.add_event_detect(24, GPIO.FALLING, callback = PlayPause, bouncetime = 2000)
 
 # Now wait!
 while 1:
