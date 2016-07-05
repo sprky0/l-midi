f8f0b4477f124e01c813875a1d89f5528899b045
Avery Brooks <averyjamesbrooks@gmail.com>
2016-07-05 06:27:45 -0400

    wtf, fix weird merge or brain aneurism

--- scripts/gpio.py
+++ scripts/gpio.py
@@ -41,15 +41,6 @@ def SendMessageToP5(message):
         print >>sys.stderr, 'closing socket'
         sock.close()
 
-    # Create a TCP/IP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # Connect the socket to the port where the server is listening
-    server_address = ('localhost', 7070)
-    print >>sys.stderr, 'connecting to %s port %s' % server_address
-    sock.connect(server_address)
-    print >>sys.stderr, 'sending playpause'
-    sock.write('playpause\n')
-
 def PlayPause(channel):
     SendMessageToP5('playpause');
 
@@ -58,10 +49,6 @@ def NextSequence(channel):
 
 def PrevSequence(channel):
     SendMessageToP5('next');
-
-# Add our function to execute when the button pressed event happens
-GPIO.add_event_detect(18, GPIO.FALLING, callback = Shutdown, bouncetime = 2000)
-GPIO.add_event_detect(24, GPIO.FALLING, callback = PlayPause, bouncetime = 2000)
 
 # Now wait!
 while 1:
