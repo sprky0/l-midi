4b0e79a322df7c3475250592e4082eb1c85539a5
Avery Brooks <averyjamesbrooks@gmail.com>
2016-07-19 09:32:13 -0400

    remove logging to avoid smashed pi at long installation

--- scripts/lmidi-start.sh
+++ scripts/lmidi-start.sh
@@ -3,6 +3,6 @@ cd /home/pi/l-midi/processing
 COUNTER=0
 while [ 1 ]; do
     echo Run $COUNTER
-    DISPLAY=:0 processing-java --sketch=lmidi --present > /tmp/lmidi.log 2>&1
+    DISPLAY=:0 processing-java --sketch=lmidi --present
     let COUNTER=COUNTER+1
 done
